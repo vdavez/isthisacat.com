fad34914d250711f0318e1b388090cd58144067e
V David Zvenyach <dave@esq.io>
2019-12-12 21:54:56 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ISTHISACAT.COM
+
+Inspired by [isitchristmas.com](https://isitchristmas.com) and [Mobilenet](https://github.com/tensorflow/tfjs-examples/tree/master/mobilenet), this app lets a user upload a picture, and the computer will tell you whether it's a cat.
---
+++ index.js
@@ -0,0 +1,149 @@
+import * as tf from '@tensorflow/tfjs';
+
+import {IMAGENET_CLASSES} from './imagenet_classes';
+
+const MOBILENET_MODEL_PATH =
+    'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
+
+const IMAGE_SIZE = 224;
+const TOPK_PREDICTIONS = 5;
+
+let mobilenet;
+const mobilenetDemo = async () => {
+
+  mobilenet = await tf.loadLayersModel(MOBILENET_MODEL_PATH);
+
+  // Warmup the model. This isn't necessary, but makes the first prediction
+  // faster. Call `dispose` to release the WebGL memory allocated for the return
+  // value of `predict`.
+  mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+
+  // Make a prediction through the locally hosted cat.jpg.
+  const catElement = document.getElementById('cat');
+  if (catElement.complete && catElement.naturalHeight !== 0) {
+    predict(catElement);
+    catElement.style.display = '';
+  } else {
+    catElement.onload = () => {
+      predict(catElement);
+      catElement.style.display = '';
+    }
+  }
+
+  document.getElementById('file-container').style.display = '';
+};
+
+/**
+ * Given an image element, makes a prediction through mobilenet returning the
+ * probabilities of the top K classes.
+ */
+async function predict(imgElement) {
+
+  // The first start time includes the time it takes to extract the image
+  // from the HTML and preprocess it, in additon to the predict() call.
+  const startTime1 = performance.now();
+  // The second start time excludes the extraction and preprocessing and
+  // includes only the predict() call.
+  let startTime2;
+  const logits = tf.tidy(() => {
+    // tf.browser.fromPixels() returns a Tensor from an image element.
+    const img = tf.browser.fromPixels(imgElement).toFloat();
+
+    const offset = tf.scalar(127.5);
+    // Normalize the image from [0, 255] to [-1, 1].
+    const normalized = img.sub(offset).div(offset);
+
+    // Reshape to a single-element batch so we can pass it to predict.
+    const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+
+    startTime2 = performance.now();
+    // Make a prediction through mobilenet.
+    return mobilenet.predict(batched);
+  });
+
+  // Convert logits to probabilities and class names.
+  const classes = await getTopKClasses(logits, TOPK_PREDICTIONS);
+  // Show the classes in the DOM.
+  console.log(classes)
+  showResults(imgElement, classes);
+}
+
+/**
+ * Computes the probabilities of the topK classes given logits by computing
+ * softmax to get probabilities and then sorting the probabilities.
+ * @param logits Tensor representing the logits from MobileNet.
+ * @param topK The number of top predictions to show.
+ */
+export async function getTopKClasses(logits, topK) {
+  const values = await logits.data();
+
+  const valuesAndIndices = [];
+  for (let i = 0; i < values.length; i++) {
+    valuesAndIndices.push({value: values[i], index: i});
+  }
+  valuesAndIndices.sort((a, b) => {
+    return b.value - a.value;
+  });
+  const topkValues = new Float32Array(topK);
+  const topkIndices = new Int32Array(topK);
+  for (let i = 0; i < topK; i++) {
+    topkValues[i] = valuesAndIndices[i].value;
+    topkIndices[i] = valuesAndIndices[i].index;
+  }
+
+  const topClassesAndProbs = [];
+  for (let i = 0; i < topkIndices.length; i++) {
+    topClassesAndProbs.push({
+      classIndex: topkIndices[i],
+      className: IMAGENET_CLASSES[topkIndices[i]],
+      probability: topkValues[i]
+    })
+  }
+  return topClassesAndProbs;
+}
+
+//
+// UI
+//
+
+function showResults(imgElement, classes) {
+  let cat = classes.filter(d => {
+    return [281, 282, 283, 284, 285].includes(d.classIndex)
+  }).length > 0;
+  let result = cat ? "YES" : "NO";
+  console.log(imgElement)
+
+  predictionElement.innerText =  result;
+  let catElement = document.getElementById('cat');
+  catElement.replaceWith(imgElement);
+}
+
+const filesElement = document.getElementById('files');
+filesElement.addEventListener('change', evt => {
+  let files = evt.target.files;
+  // Display thumbnails & issue call to predict each image.
+  for (let i = 0, f; f = files[i]; i++) {
+    // Only process image files (skip non image files)
+    if (!f.type.match('image.*')) {
+      continue;
+    }
+    let reader = new FileReader();
+    reader.onload = e => {
+      // Fill the image & call predict.
+      let img = document.createElement('img');
+      img.id = 'cat';
+      img.className = 'mx-auto';
+      img.src = e.target.result;
+      img.width = IMAGE_SIZE;
+      img.height = IMAGE_SIZE;
+      img.onload = () => predict(img);
+    };
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+  }
+});
+
+const predictionElement = document.getElementById('prediction');
+
+mobilenetDemo();
